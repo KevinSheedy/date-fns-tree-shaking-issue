19a236759c47399d371343837d11298530cbbd57
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2019-08-16 18:04:22 +0100

    webpack babel config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+module.exports = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: './../reports/bundle-analyzer.report.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'index.js',
+    library: 'DateFnsTreeShaking',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+  target: 'web'
+};
